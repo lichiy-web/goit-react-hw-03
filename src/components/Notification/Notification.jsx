277a0a85c9f50dc2ac850742ec688f74918ce405
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-01-26 19:14:38 +0200

    change input styles

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -3,8 +3,8 @@ import css from './Notification.module.css';
 const Notification = () => {
   return (
     <div className={css.notification}>
-      There are no contacts in the phonebook. Please add a first one using form
-      above!
+      There are no contacts in the phonebook. <br />
+      Please add the first one using form above!
     </div>
   );
 };
